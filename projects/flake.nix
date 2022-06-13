6d6801f50574f00dda22efb5c2a0e56ff796148d
Yannik Sander <yannik@floxdev.com>
2022-06-13 16:03:22 +0200

    Add functional Friends Subchannel

--- projects/flake.nix
+++ projects/flake.nix
@@ -1,5 +1,7 @@
 rec {
 
+  inputs.functional-friends.url = "github:flox-examples/functional-friends";
+  inputs.functional-friends.inputs.capacitor.follows = "capacitor";
 
   outputs = {self, capacitor, ...} @ inputs: inputs ;
 
